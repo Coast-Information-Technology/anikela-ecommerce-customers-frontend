d75ef54eb9fc4eefa2775d0a90a1999f6b02f90b
Eniola Ajayi <ajayieniola16@gmail.com>
2020-12-30 00:26:54 +0100

    modified header component and App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <BrowserRouter>
-      <></>
+      <Header />
+      <div></div>
     </BrowserRouter>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header>
+      <MiniNav />
+    </header>
+  );
 };
 
 const MiniNav = () => {
   return (
     <nav className="">
       <span className="">
-        <span className="text-primary">Location</span>
+        <span className="text-primary ">Location</span>
         {/* still requires a dropdown for site currency conversion */}
       </span>
-      <span className="text-primary">Care-line +09074348485</span>
+      <span className="fs-1 text-primary ">Care-line +09074348485</span>
     </nav>
   );
 };
+
+export default Header;
