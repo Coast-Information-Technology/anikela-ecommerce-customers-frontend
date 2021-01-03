ddb81660a20f03fc0e8ef54e78d0b79d07bc1da2
Edmund <eddyfhizzy2000@gmail.com>
2021-01-03 17:30:39 +0100

    added design for homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/header/Header";
+import Main from "./pages/signin_signup/signin"
+import Homepage from "./pages/homepage/Homepage"
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
+      {/* <Main /> */}
+      <Homepage />
       <div></div>
     </BrowserRouter>
   );
