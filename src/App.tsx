42030f11cd8e2ecf7c59267a181d7d19dad85208
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-03 19:19:36 +0100

    added spacing to better visualize the footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <div></div>
+      <div style={{ height: "600px" }}></div>
+      <Footer />
     </BrowserRouter>
   );
 }
