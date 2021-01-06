7d09dd2f04ba901d75eecc49bd45ce01c94f40c4
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-06 14:15:35 +0100

    fixed header responsivity

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -1,63 +1,75 @@
 import React from "react";
+import MediaQuery from "react-responsive";
 import { Link } from "react-router-dom";
 import logo from "../../assets/resources/logo.svg";
 import Icon from "../common/icon/Icon";
 
-const NavBar: React.FC = () => {
+const SideNavToggle: React.FC = () => {
   const openSideNav = () => {
-    alert("yeehaw!!");
+    alert("Hello! Your Side Nav will soon finish rendering....");
   };
+
+  return (
+    <div
+      className="btn px-0"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+      onClick={openSideNav}
+    >
+      <svg viewBox="-5 0 10 8" width="40" className="navbar-toggler-icon">
+        <line
+          y2="8"
+          stroke="rgb(1, 2, 12)"
+          strokeWidth="30"
+          strokeDasharray="1 2"
+        />
+      </svg>
+    </div>
+  );
+};
+
+const NavBar: React.FC = () => {
   const noOfItemsInCart: number = 12;
   return (
     <nav className="navbar">
-      <div className="container">
-        <div
-          className="btn navbar-toggler flex-shrink-1"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          onClick={openSideNav}
-        >
-          <svg viewBox="-5 0 10 8" width="20" className="navbar-toggler-icon">
-            <line
-              y2="8"
-              stroke="rgb(1, 2, 12)"
-              stroke-width="20"
-              stroke-dasharray="1 2"
-            />
-          </svg>
-        </div>
-
-        <Link to="/" className="navbar-brand">
+      <div className="container-lg">
+        <MediaQuery maxDeviceWidth={1023}>
+          <SideNavToggle />
+        </MediaQuery>
+        <Link to="/" className="">
           <img src={logo} alt="Anikela logo" className="logo" />
         </Link>
+        <MediaQuery minDeviceWidth={1024}>
+          <div className="navbar-nav flex-row flex-grow-1 justify-content-evenly">
+            <Link to="products/women" className="nav-link">
+              women
+            </Link>
+            <Link to="products/men" className="nav-link">
+              men
+            </Link>
+          </div>
+        </MediaQuery>
 
-        <div className="navbar-nav">
-          <Link to="products/women" className="nav-link">
-            women
-          </Link>
-          <Link to="products/men" className="nav-link">
-            men
-          </Link>
-        </div>
+        <MediaQuery minDeviceWidth={576}>
+          <div className="nav-util">
+            <Link to="dashboard">
+              <Icon icon="user" title="your account" className="" size={24} />
+            </Link>
+          </div>
 
-        <div className="nav-util flex-shrink-1">
-          <Link to="dashboard">
-            <Icon icon="user" title="your account" className="" size={24} />
-          </Link>
-        </div>
+          <div className="nav-util">
+            <Link to="wishlist" className="">
+              <Icon
+                icon="wishlist"
+                title="Your wishlist"
+                className="nav-util__img"
+                size={24}
+              />
+            </Link>
+          </div>
+        </MediaQuery>
 
-        <div className="nav-util flex-shrink-1">
-          <Link to="wishlist" className="">
-            <Icon
-              icon="wishlist"
-              title="Your wishlist"
-              className="nav-util__img"
-              size={24}
-            />
-          </Link>
-        </div>
-
-        <div className="nav-util position-relative btn flex-shrink-1">
+        <div className="nav-util position-relative">
           <Link to="shopping-cart">
             <Icon
               icon="shopping-bag"
@@ -66,7 +78,7 @@ const NavBar: React.FC = () => {
               size={24}
             />
           </Link>
-          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill text-primary bg-secondary">
+          <span className="position-absolute btn top-0 start-100 translate-middle badge rounded-pill text-primary bg-secondary">
             {noOfItemsInCart.toString()}
             <span className="visually-hidden">items in cart</span>
           </span>
