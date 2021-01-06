f43203337cb1e98b686838b707d9bfdd9a1fad13
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-06 14:19:14 +0100

    imported side nav toggle component

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -3,30 +3,7 @@ import MediaQuery from "react-responsive";
 import { Link } from "react-router-dom";
 import logo from "../../assets/resources/logo.svg";
 import Icon from "../common/icon/Icon";
-
-const SideNavToggle: React.FC = () => {
-  const openSideNav = () => {
-    alert("Hello! Your Side Nav will soon finish rendering....");
-  };
-
-  return (
-    <div
-      className="btn px-0"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-      onClick={openSideNav}
-    >
-      <svg viewBox="-5 0 10 8" width="40" className="navbar-toggler-icon">
-        <line
-          y2="8"
-          stroke="rgb(1, 2, 12)"
-          strokeWidth="30"
-          strokeDasharray="1 2"
-        />
-      </svg>
-    </div>
-  );
-};
+import SideNavToggle from "../common/mobile_nav/SideNavToggle";
 
 const NavBar: React.FC = () => {
   const noOfItemsInCart: number = 12;
