d39fc1506fb1c68a8f4b436fe75d82d24574d61a
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-06 01:08:54 +0100

    refactored mini nav component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,19 +4,21 @@ import NavBar from "./NavBar";
 const Header = () => {
   return (
     <header className="header">
-      <MiniNav />
+      <MiniNavBar />
       <NavBar />
     </header>
   );
 };
 
-const MiniNav: React.FC = () => {
+const MiniNavBar: React.FC = () => {
   return (
-    <nav className="mini-nav">
-      <span className="mini-nav__location">
-        <span className="location-dropdown__text">Location</span>
-      </span>
-      <span className="text-primary ">Care-line +09074348485</span>
+    <nav className="mini-navbar bg-dark">
+      <div className="container d-flex justify-content-between py-1">
+        <span className="mini-navbar__location">
+          <span className="text-primary">Location</span>
+        </span>
+        <span className="text-primary ">Care-line +09074348485</span>
+      </div>
     </nav>
   );
 };
