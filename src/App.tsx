52012ec90c9b47deb2fbb8dbf5fbb0b00286ed3a
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-06 14:31:05 +0100

    removed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-import Main from "./pages/signin_signup/signin";
 import Homepage from "./pages/homepage/Homepage";
 
 function App() {
