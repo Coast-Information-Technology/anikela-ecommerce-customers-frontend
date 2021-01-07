fa2d0d6da2ebee7eec8ae2209aba3cc6169f06ed
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-07 20:35:45 +0100

    defined structure for bottom nav component

--- src/components/common/mobile_nav/BottomNav.tsx
+++ src/components/common/mobile_nav/BottomNav.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Icon from "../icon/Icon";
 
 const BottomNav: React.FC = () => {
-  return <div></div>;
+  return (
+    <nav className="bottom-nav">
+      <Link to="/" className="bottom-nav__btn">
+        <Icon icon="shop" title="shop" className="bottom-nav__icon" />
+        <span className="bottom-nav__text">shop</span>
+      </Link>
+      <Link to="/" className="bottom-nav__btn">
+        <Icon icon="search" title="search" className="bottom-nav__icon" />
+        <span className="bottom-nav__text">search</span>
+      </Link>
+      <Link to="/" className="bottom-nav__btn">
+        <Icon icon="wishlist" title="wishlist" className="bottom-nav__icon" />
+        <span className="bottom-nav__text">wishlist</span>
+      </Link>
+      <Link to="/" className="bottom-nav__btn">
+        <Icon icon="user" title="account" className="bottom-nav__icon" />
+        <span className="bottom-nav__text">account</span>
+      </Link>
+    </nav>
+  );
 };
 
 export default BottomNav;
