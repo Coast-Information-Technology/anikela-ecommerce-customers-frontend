48312b75387d5f4f23cc1552f274010f7af4a3a9
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-07 21:21:18 +0100

    added sizes for icons

--- src/components/common/mobile_nav/BottomNav.tsx
+++ src/components/common/mobile_nav/BottomNav.tsx
@@ -6,19 +6,34 @@ const BottomNav: React.FC = () => {
   return (
     <nav className="bottom-nav">
       <Link to="/" className="bottom-nav__btn">
-        <Icon icon="shop" title="shop" className="bottom-nav__icon" />
+        <Icon icon="shop" title="shop" size={18} className="bottom-nav__icon" />
         <span className="bottom-nav__text">shop</span>
       </Link>
       <Link to="/" className="bottom-nav__btn">
-        <Icon icon="search" title="search" className="bottom-nav__icon" />
+        <Icon
+          icon="search"
+          title="search"
+          size={18}
+          className="bottom-nav__icon"
+        />
         <span className="bottom-nav__text">search</span>
       </Link>
       <Link to="/" className="bottom-nav__btn">
-        <Icon icon="wishlist" title="wishlist" className="bottom-nav__icon" />
+        <Icon
+          icon="wishlist"
+          title="wishlist"
+          size={18}
+          className="bottom-nav__icon"
+        />
         <span className="bottom-nav__text">wishlist</span>
       </Link>
       <Link to="/" className="bottom-nav__btn">
-        <Icon icon="user" title="account" className="bottom-nav__icon" />
+        <Icon
+          icon="user"
+          title="account"
+          size={18}
+          className="bottom-nav__icon"
+        />
         <span className="bottom-nav__text">account</span>
       </Link>
     </nav>
