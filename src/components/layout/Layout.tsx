dbf2f7ffd7ec3f1e618274f4d69c0996e6287daf
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-08 16:28:31 +0100

    initializing layout component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
+import MediaQuery from "react-responsive";
+import { BrowserRouter } from "react-router-dom";
+import BottomNav from "../common/mobile_nav/BottomNav";
+import SideNav from "../common/mobile_nav/SideNav";
+import Footer from "../footer/Footer";
+import Header from "../header/Header";
 
 class Layout extends Component {
   render() {
-    return <div></div>;
+    return (
+      <>
+        <BrowserRouter>
+          <MediaQuery maxDeviceWidth={1024}>
+            <SideNav />
+          </MediaQuery>
+          <Header />
+          <MediaQuery maxDeviceWidth={375}>
+            <BottomNav />
+          </MediaQuery>
+          <Footer />
+        </BrowserRouter>
+      </>
+    );
   }
 }
 
