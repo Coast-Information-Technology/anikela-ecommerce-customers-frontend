1828581f9869054eaf47bad5e1b71b39bed9734e
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-08 16:49:53 +0100

    added default content to side nav

--- src/components/common/mobile_nav/SideNav.tsx
+++ src/components/common/mobile_nav/SideNav.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Icon from "../icon/Icon";
 
 const SideNav: React.FC = () => {
-  return <aside></aside>;
+  return (
+    <aside className="side-nav">
+      <h6>Side Nav</h6>
+      <Icon icon="close" title="close" size={24} fill="rgb(0,0,0)" />
+    </aside>
+  );
 };
 
 export default SideNav;
