02342fe6701768bf17be7c69064edfe1dc0a1a8b
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-08 17:17:04 +0100

    created contain class for sidenav

--- src/components/common/mobile_nav/SideNav.tsx
+++ src/components/common/mobile_nav/SideNav.tsx
@@ -3,10 +3,12 @@ import Icon from "../icon/Icon";
 
 const SideNav: React.FC = () => {
   return (
-    <aside className="side-nav">
-      <h6>Side Nav</h6>
-      <Icon icon="close" title="close" size={24} fill="rgb(0,0,0)" />
-    </aside>
+    <div className="contain">
+      <aside className="side-nav">
+        <h6>Side Nav</h6>
+        <Icon icon="close" title="close" size={24} fill="rgb(0,0,0)" />
+      </aside>
+    </div>
   );
 };
 
