01fe2339cab0a45846260dce27e5d55b28df76fe
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-10 20:45:53 +0100

    fixed import file path for redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import store from "./state/store";
 import App from "./App";
 import "./scss/main.scss";
 import "bootstrap/dist/js/bootstrap.bundle";
