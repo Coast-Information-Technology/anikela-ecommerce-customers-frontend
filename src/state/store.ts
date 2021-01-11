b6f4d626abb4c418fa235d5bb90d4e290dc21da9
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-11 11:08:04 +0100

    added re-usable type for thunks

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,7 +1,15 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./rootReducer";
+import { createStore, applyMiddleware, Action } from "redux";
+import thunk, { ThunkAction } from "redux-thunk";
+import rootReducer, { RootState } from "./rootReducer";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
+
+// Re usable thunk type
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
 
 export default store;
