dccd63ae8626e8d2886c1be26f330e29f0e3260e
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-13 21:54:02 +0100

    removed footer cta section

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,128 +10,101 @@ import mcImage from "../../assets/resources/mastercard.svg";
 
 const Footer: React.FC = () => {
   return (
-    <>
-      <FooterCtaSection />
-      <footer className="footer">
-        <div className="footer-content container">
-          <div className="footer-group__container">
-            <ul className="footer-group">
-              <li className="footer-group__header">Need some help</li>
-              <li className="footer-group__item">
-                <Link to="" className="footer-group__link">
-                  help
-                </Link>
-              </li>
-              <li className="footer-group__item">
-                <Link to="" className="footer-group__link">
-                  track order
-                </Link>
-              </li>
-              <li className="footer-group__item">
-                <Link to="" className="footer-group__link">
-                  delivery and return policies
-                </Link>
-              </li>
-              <li className="footer-group__item">
-                <Link to="" className="footer-group__link">
-                  customer service
-                </Link>
-              </li>
-              <li className="footer-group__item">
-                <Link to="" className="footer-group__link">
-                  FAQ
-                </Link>
-              </li>
-            </ul>
-            <ul className="footer-group">
-              <li className="footer-group__header">About Anikela</li>
-              <li className="footer-group__item">
-                <Link to="" className="footer-group__link">
-                  about us
-                </Link>
-              </li>
-              <li className="footer-group__item">
-                <Link to="" className="footer-group__link">
-                  careers at anikela
-                </Link>
-              </li>
-              <li className="footer-group__item">
-                <Link to="" className="footer-group__link">
-                  investors
-                </Link>
-              </li>
-              <li className="footer-group__item">
-                <Link to="" className="footer-group__link">
-                  terms and condition
-                </Link>
-              </li>
-            </ul>
-            <ul className="footer-group">
-              <li className="footer-group__header">more to know</li>
-              <li className="footer-group__item">
-                <Link to="" className="footer-group__link">
-                  contact us
-                </Link>
-              </li>
-              <li className="footer-group__item">
-                <Link to="" className="footer-group__link">
-                  gift vouchers
-                </Link>
-              </li>
-              <li className="footer-group__item">
-                <Link to="" className="footer-group__link">
-                  anikela brands
-                </Link>
-              </li>
-              <li className="footer-group__item">
-                <Link to="" className="footer-group__link">
-                  coupons
-                </Link>
-              </li>
-            </ul>
-            <ul className="footer-group">
-              <li className="footer-group__header">
-                sign up to our newsletter
-              </li>
-              <p className="footer-group__lead">
-                get the first looks at new drops,events,sales & offers
-              </p>
-              <label htmlFor={"subscribe"} className="visually-hidden">
-                Enter your email address
-              </label>
-              <input
-                id="subscribe"
-                type="text"
-                className="footer-group__input"
-              />
-              <button type="submit" className="footer-group__submit">
-                subscribe
-              </button>
-            </ul>
-          </div>
-          <FooterIconSection />
-          <FooterFootNote />
+    <footer className="footer">
+      <div className="footer-content container">
+        <div className="footer-group__container">
+          <ul className="footer-group">
+            <li className="footer-group__header">Need some help</li>
+            <li className="footer-group__item">
+              <Link to="" className="footer-group__link">
+                help
+              </Link>
+            </li>
+            <li className="footer-group__item">
+              <Link to="" className="footer-group__link">
+                track order
+              </Link>
+            </li>
+            <li className="footer-group__item">
+              <Link to="" className="footer-group__link">
+                delivery and return policies
+              </Link>
+            </li>
+            <li className="footer-group__item">
+              <Link to="" className="footer-group__link">
+                customer service
+              </Link>
+            </li>
+            <li className="footer-group__item">
+              <Link to="" className="footer-group__link">
+                FAQ
+              </Link>
+            </li>
+          </ul>
+          <ul className="footer-group">
+            <li className="footer-group__header">About Anikela</li>
+            <li className="footer-group__item">
+              <Link to="" className="footer-group__link">
+                about us
+              </Link>
+            </li>
+            <li className="footer-group__item">
+              <Link to="" className="footer-group__link">
+                careers at anikela
+              </Link>
+            </li>
+            <li className="footer-group__item">
+              <Link to="" className="footer-group__link">
+                investors
+              </Link>
+            </li>
+            <li className="footer-group__item">
+              <Link to="" className="footer-group__link">
+                terms and condition
+              </Link>
+            </li>
+          </ul>
+          <ul className="footer-group">
+            <li className="footer-group__header">more to know</li>
+            <li className="footer-group__item">
+              <Link to="" className="footer-group__link">
+                contact us
+              </Link>
+            </li>
+            <li className="footer-group__item">
+              <Link to="" className="footer-group__link">
+                gift vouchers
+              </Link>
+            </li>
+            <li className="footer-group__item">
+              <Link to="" className="footer-group__link">
+                anikela brands
+              </Link>
+            </li>
+            <li className="footer-group__item">
+              <Link to="" className="footer-group__link">
+                coupons
+              </Link>
+            </li>
+          </ul>
+          <ul className="footer-group">
+            <li className="footer-group__header">sign up to our newsletter</li>
+            <p className="footer-group__lead">
+              get the first looks at new drops,events,sales & offers
+            </p>
+            <label htmlFor={"subscribe"} className="visually-hidden">
+              Enter your email address
+            </label>
+            <input id="subscribe" type="text" className="footer-group__input" />
+            <button type="submit" className="footer-group__submit">
+              subscribe
+            </button>
+          </ul>
         </div>
-      </footer>
-    </>
-  );
-};
-
-const FooterCtaSection: React.FC = () => {
-  return (
-    <div className="footer-cta">
-      <div className="footer-cta-section container">
-        <div role="button" className="footer-cta-section__btn">
-          Shop the collection now
-        </div>
-        <div className="footer-cta-section__lead">
-          Discounted prices you can trust. and some text for testing
-        </div>
-        <div role="button" className="footer-cta-section__btn">
-          Shop the collection now
-        </div>
+        <FooterIconSection />
+        <FooterFootNote />
       </div>
-    </div>
+    </footer>
   );
 };
 
