16dd2beb0a1026d30762291429f46aa2388f55d7
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-13 21:54:32 +0100

    created sections file

---
+++ src/components/layout/Sections.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const FooterCtaSection: React.FC = () => {
+  return (
+    <div className="footer-cta">
+      <div className="footer-cta-section container">
+        <button className="footer-cta-section__btn">
+          Shop the collection now
+        </button>
+        <div className="footer-cta-section__lead">
+          Discounted prices you can trust. and some text for testing
+        </div>
+        <button className="footer-cta-section__btn">
+          Shop the collection now
+        </button>
+      </div>
+    </div>
+  );
+};
