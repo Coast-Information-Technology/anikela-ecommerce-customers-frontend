9210653da67a2d56bd9033dd49d2799ce5466840
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-13 21:55:06 +0100

    added footer cta section to layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,6 +5,7 @@ import BottomNav from "../common/mobile_nav/BottomNav";
 import SideNav from "../common/mobile_nav/SideNav";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
+import { FooterCtaSection } from "./Sections";
 
 class Layout extends Component {
   state = {
@@ -23,6 +24,7 @@ class Layout extends Component {
         <MediaQuery maxDeviceWidth={375}>
           <BottomNav />
         </MediaQuery>
+        <FooterCtaSection />
         <Footer />
       </BrowserRouter>
     );
