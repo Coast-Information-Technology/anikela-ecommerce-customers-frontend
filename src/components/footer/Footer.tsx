987344eb0f7e81ffd0a46c5ab6794dd8638b51b5
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-13 20:40:07 +0100

    added all footer icons to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Icon from "../common/icon/Icon";
+
+// assets
+import paypalImage from "../../assets/resources/paypal.svg";
+import aeImage from "../../assets/resources/american express.svg";
+import visaImage from "../../assets/resources/visa.svg";
+import mcImage from "../../assets/resources/mastercard.svg";
 
 const Footer: React.FC = () => {
   return (
@@ -102,7 +109,7 @@ const Footer: React.FC = () => {
               </button>
             </ul>
           </div>
-          <div className="footer-icons"></div>
+          <FooterIconSection />
           <FooterFootNote />
         </div>
       </footer>
@@ -123,6 +130,69 @@ const FooterCtaSection: React.FC = () => {
         <div role="button" className="footer-cta-section__btn">
           Shop the collection now
         </div>
+      </div>
+    </div>
+  );
+};
+
+const FooterIconSection: React.FC = () => {
+  return (
+    <div className="footer-icons">
+      <div className="footer-icons__social">
+        <a href="https://www.facebook.com/" className="footer-icon facebook">
+          <Icon
+            icon="facebook"
+            title="like us on facebook"
+            fill="rgb(68,96,160)"
+            viewBox="0 0 32 32"
+            size={18}
+          />
+        </a>
+        <a href="https://www.instagram.com/" className="footer-icon instagram">
+          <Icon
+            icon="instagram"
+            title="heart us on instagram"
+            fill="rgb(221, 42, 123)"
+            viewBox="0 0 169 169"
+            size={18}
+          />
+        </a>
+        <a href="https://www.twitter.com/" className="footer-icon twitter">
+          <Icon
+            icon="twitter"
+            title="follow us on twitter"
+            fill="rgb(0,170,236)"
+            viewBox="0 0 32 26.777"
+            size={18}
+          />
+        </a>
+        <a href="https://www.pinterest.com/" className="footer-icon pinterest">
+          <Icon
+            icon="pinterest"
+            title="follow us on pinterest"
+            fill="rgb(204,33,39)"
+            viewBox="0 0 32 32"
+            size={18}
+          />
+        </a>
+      </div>
+      <div className="footer-icons__payment">
+        <img
+          src={paypalImage}
+          alt="paypal"
+          className="footer-icon__img paypal"
+        />
+        <img
+          src={aeImage}
+          alt="american-express"
+          className="footer-icon__img american-express"
+        />
+        <img src={visaImage} alt="visa" className="footer-icon__img visa" />
+        <img
+          src={mcImage}
+          alt="master-card"
+          className="footer-icon__img mastercard"
+        />
       </div>
     </div>
   );
