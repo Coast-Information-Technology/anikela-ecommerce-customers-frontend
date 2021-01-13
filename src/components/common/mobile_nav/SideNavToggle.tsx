70ef24a0956f94e04426d7d30c74b93986bcfef3
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-13 22:36:56 +0100

    fixed menu nav icon

--- src/components/common/mobile_nav/SideNavToggle.tsx
+++ src/components/common/mobile_nav/SideNavToggle.tsx
@@ -12,13 +12,10 @@ const SideNavToggle: React.FC = () => {
       aria-label="Toggle navigation"
       onClick={openSideNav}
     >
-      <svg viewBox="-5 0 10 8" width="40" className="navbar-toggler-icon">
-        <line
-          y2="8"
-          stroke="rgb(1, 2, 12)"
-          strokeWidth="30"
-          strokeDasharray="1 2"
-        />
+      <svg height="18" viewBox="0 -53 384 384" width="18">
+        <path d="m368 154.667969h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0" />
+        <path d="m368 32h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0" />
+        <path d="m368 277.332031h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0" />
       </svg>
     </div>
   );
