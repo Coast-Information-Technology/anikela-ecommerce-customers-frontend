6c19d9ee3fa0f62df2157ae5746b857f5a6512de
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-14 23:00:54 +0100

    created side nav Reducer

---
+++ src/state/sideNav/sideNavReducer.ts
@@ -0,0 +1,23 @@
+import { OPEN, CLOSE, SideNavActionTypes, SideNavState } from "./sideNavTypes";
+
+const initialState: SideNavState = {
+  isOpen: false,
+};
+
+const sideNavReducer = (
+  state = initialState,
+  action: SideNavActionTypes
+): SideNavState => {
+  switch (action.type) {
+    case OPEN:
+      return {
+        isOpen: action.payload,
+      };
+    case CLOSE:
+      return {
+        isOpen: action.payload,
+      };
+    default:
+      return state;
+  }
+};
