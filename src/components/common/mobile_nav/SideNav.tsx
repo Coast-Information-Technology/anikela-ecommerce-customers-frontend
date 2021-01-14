453154b3ffa7e307c42d3a38c2d5ca8f49ca7947
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-14 23:40:40 +0100

    added state to side nav

--- src/components/common/mobile_nav/SideNav.tsx
+++ src/components/common/mobile_nav/SideNav.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import Icon from "../icon/Icon";
+import { connect, ConnectedProps } from "react-redux";
+import { SideNavState } from "../../../state/sideNav/sideNavTypes";
 
-const SideNav: React.FC = () => {
+const mapStateToProps = (state: SideNavState) => ({
+  isOpen: state.isSideNavOpen,
+});
+
+const mapDispatchToProps = {
+  toggleSideNavState: () => ({ type: "OPEN_SIDE_NAV" }),
+};
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+interface SideNavProps extends PropsFromRedux {}
+
+const SideNav = (props: SideNavProps) => {
   return (
     <div className="contain">
       <aside className="side-nav">
-        <h6>Side Nav</h6>
-        <Icon icon="close" title="close" size={24} fill="rgb(0,0,0)" />
+        <h6>Side Nav {props.isOpen}</h6>
+        <button onClick={props.toggleSideNavState}>
+          <Icon icon="close" title="close" size={24} fill="rgb(0,0,0)" />
+        </button>
       </aside>
     </div>
   );
 };
 
-export default SideNav;
+export default connector(SideNav);
