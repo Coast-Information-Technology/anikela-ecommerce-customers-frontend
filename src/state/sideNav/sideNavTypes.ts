01582f80f42f635bed2faf2481a4e80cad5f58d7
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-15 14:00:50 +0100

    fixed payload type

--- src/state/sideNav/sideNavTypes.ts
+++ src/state/sideNav/sideNavTypes.ts
@@ -1,17 +1,16 @@
 export const OPEN_SIDE_NAV = "OPEN_SIDE_NAV";
 export const CLOSE_SIDE_NAV = "CLOSE_SIDE_NAV";
-
 export interface SideNavState {
   isSideNavOpen: boolean;
 }
 interface OpenSideNavAction {
   type: typeof OPEN_SIDE_NAV;
-  payload: true;
+  payload: boolean;
 }
 
 interface CloseSideNavAction {
   type: typeof CLOSE_SIDE_NAV;
-  payload: false;
+  payload: boolean;
 }
 
-export type SideNavActionTypes = OpenSideNavAction | CloseSideNavAction;
+export type SideNavActionType = OpenSideNavAction | CloseSideNavAction;
