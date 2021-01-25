34f276376ad2a0c011ef5c074cbd9d769ad126f5
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-25 13:22:59 +0100

    added tab items

--- src/components/header/MegaMenu.tsx
+++ src/components/header/MegaMenu.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { TabItem, TabNavigation } from "../common/TabNavigation";
 
 const MegaMenu: React.FC = () => {
-  return <></>;
+  return (
+    <TabNavigation className="mega-menu">
+      <TabItem label="newIn" tabTitle="New In"></TabItem>
+      <TabItem label="clothing" tabTitle="Clothing">
+        <div className="tab-item__content"></div>
+      </TabItem>
+      <TabItem label="shoes" tabTitle="Shoes"></TabItem>
+      <TabItem label="accessories" tabTitle="Accessories"></TabItem>
+      <TabItem label="gifts" tabTitle="Gifts"></TabItem>
+      <TabItem label="activeWear" tabTitle="Activewear"></TabItem>
+      <TabItem label="cosmetics" tabTitle="Cosmetics"></TabItem>
+      <TabItem label="brands" tabTitle="Brands"></TabItem>
+      <TabItem label="collections" tabTitle="Collections"></TabItem>
+    </TabNavigation>
+  );
 };
 
 export default MegaMenu;
