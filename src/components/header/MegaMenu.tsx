435f5abc0e112117fedfc77b53b614b728a7325e
Eniola Ajayi <ajayieniola16@gmail.com>
2021-01-26 21:03:09 +0100

    added default mega menu items

--- src/components/header/MegaMenu.tsx
+++ src/components/header/MegaMenu.tsx
@@ -4,17 +4,33 @@ import { TabItem, TabNavigation } from "../common/TabNavigation";
 const MegaMenu: React.FC = () => {
   return (
     <TabNavigation className="mega-menu">
-      <TabItem label="newIn" tabTitle="New In"></TabItem>
-      <TabItem label="clothing" tabTitle="Clothing">
-        <div className="tab-item__content"></div>
+      <TabItem label="newIn" tabTitle="New In">
+        New In
       </TabItem>
-      <TabItem label="shoes" tabTitle="Shoes"></TabItem>
-      <TabItem label="accessories" tabTitle="Accessories"></TabItem>
-      <TabItem label="gifts" tabTitle="Gifts"></TabItem>
-      <TabItem label="activeWear" tabTitle="Activewear"></TabItem>
-      <TabItem label="cosmetics" tabTitle="Cosmetics"></TabItem>
-      <TabItem label="brands" tabTitle="Brands"></TabItem>
-      <TabItem label="collections" tabTitle="Collections"></TabItem>
+      <TabItem label="clothing" tabTitle="Clothing">
+        <div className="tab-item__content">Clothing</div>
+      </TabItem>
+      <TabItem label="shoes" tabTitle="Shoes">
+        Shoes
+      </TabItem>
+      <TabItem label="accessories" tabTitle="Accessories">
+        Accesories
+      </TabItem>
+      <TabItem label="gifts" tabTitle="Gifts">
+        Gifts
+      </TabItem>
+      <TabItem label="activeWear" tabTitle="Activewear">
+        Active wear
+      </TabItem>
+      <TabItem label="cosmetics" tabTitle="Cosmetics">
+        Cosmetics
+      </TabItem>
+      <TabItem label="brands" tabTitle="Brands">
+        Brands
+      </TabItem>
+      <TabItem label="collections" tabTitle="Collections">
+        Collections
+      </TabItem>
     </TabNavigation>
   );
 };
