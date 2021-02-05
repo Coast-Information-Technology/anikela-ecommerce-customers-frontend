6e73f5768715141bc8769b3fb93e40e3cf306752
Eniola Ajayi <ajayieniola16@gmail.com>
2021-02-06 00:25:36 +0100

    changed classnames

--- src/pages/FourZeroFour.tsx
+++ src/pages/FourZeroFour.tsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 
 const FourZeroFour: React.FC = () => {
   return (
-    <section className="404">
-      <span className="404-exclamation">Oopps!</span>
+    <section className="f04">
+      <span className="f04-exclamation">Oopps!</span>
 
-      <div className="404-content">
-        <h4 className="404-content__heading">This page dose not exist</h4>
-        <p className="404-content__lead">
+      <div className="f04-content">
+        <h4 className="f04-content__heading">This page dose not exist</h4>
+        <p className="f04-content__lead">
           it's okay don't fret you can either go to the
-          <Link to="/" className="404-content__link">
+          <Link to="/" className="f04-content__link">
             homepage
           </Link>
           or
-          <Link to="/" className="404-content__link">
+          <Link to="/" className="f04-content__link">
             send us a message
           </Link>
           about the broken link
