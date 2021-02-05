37fe5209b9d72d75d65475ce625b76ba22403a17
Eniola Ajayi <ajayieniola16@gmail.com>
2021-02-06 00:21:59 +0100

    added simple 404 structure

--- src/pages/FourZeroFour.tsx
+++ src/pages/FourZeroFour.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { CSSProperties } from "react";
+import { Link } from "react-router-dom";
 
 const FourZeroFour: React.FC = () => {
   return (
-    <div>
-      <h1> Welocome to 404 lol.... mumu</h1>
-    </div>
+    <section className="404">
+      <span className="404-exclamation">Oopps!</span>
+
+      <div className="404-content">
+        <h4 className="404-content__heading">This page dose not exist</h4>
+        <p className="404-content__lead">
+          it's okay don't fret you can either go to the
+          <Link to="/" className="404-content__link">
+            homepage
+          </Link>
+          or
+          <Link to="/" className="404-content__link">
+            send us a message
+          </Link>
+          about the broken link
+        </p>
+      </div>
+    </section>
   );
 };
 
