6bca08ea10726f8a70a2cdf1235512b0bdcf8dea
Eniola Ajayi <ajayieniola16@gmail.com>
2021-02-06 00:05:56 +0100

    created 404 page component

---
+++ src/pages/FourZeroFour.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FourZeroFour: React.FC = () => {
+  return (
+    <div>
+      <h1> Welocome to 404 lol.... mumu</h1>
+    </div>
+  );
+};
+
+export default FourZeroFour;
