d57905ad4ed0ddafbadbd9d0a8f4aab77b0b3c91
Eniola Ajayi <ajayieniola16@gmail.com>
2021-02-06 01:25:50 +0100

    modified 404 page

--- src/pages/FourZeroFour.tsx
+++ src/pages/FourZeroFour.tsx
@@ -1,24 +1,32 @@
-import React, { CSSProperties } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 const FourZeroFour: React.FC = () => {
   return (
     <section className="f04">
-      <span className="f04-exclamation">Oopps!</span>
+      <div className="f04-container">
+        <span className="f04-exclamation">Oopps!</span>
+        <div className="f04-error-num">
+          <span>4</span>
+          <span>0</span>
+          <span>4</span>
+        </div>
 
-      <div className="f04-content">
-        <h4 className="f04-content__heading">This page dose not exist</h4>
-        <p className="f04-content__lead">
-          it's okay don't fret you can either go to the
-          <Link to="/" className="f04-content__link">
-            homepage
-          </Link>
-          or
-          <Link to="/" className="f04-content__link">
-            send us a message
-          </Link>
-          about the broken link
-        </p>
+        <div className="f04-content">
+          <h4 className="f04-content__heading">This page does not exist</h4>
+          <p className="f04-content__lead">
+            it's okay don't fret you can either go to the{" "}
+            <Link to="/" className="f04-content__link">
+              homepage
+            </Link>{" "}
+            or{" "}
+            <Link to="/" className="f04-content__link">
+              {" "}
+              send us a message{" "}
+            </Link>{" "}
+            about the broken link
+          </p>
+        </div>
       </div>
     </section>
   );
