220dae3d3033ff88ba415657ab57a424b551d4a9
Eniola Ajayi <ajayieniola16@gmail.com>
2021-02-14 15:20:07 +0100

    added user auth page to route

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
 import CTA from "./pages/CTA page/CTA";
 import FourZeroFour from "./pages/FourZeroFour";
+import { UserAuthPage } from "./pages/user-auth page/UserAuthPage";
 // import male_cta from "./pages/CTA page/male_cta"
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Homepage} exact></Route>
+        <Route path="/userauth" component={UserAuthPage}></Route>
         <Route path="/products/men" component={CTA}></Route>
         <Route path="/products/women" component={CTA}></Route>
+        <Route path="/" component={Homepage} exact></Route>
         <Route path="*" component={FourZeroFour}></Route>
       </Switch>
     </Router>
